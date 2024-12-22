5fd7401fc40fe2195d24cd43f266c5bf7028e41d
Rasik Bhattarai <rasikbhattarai48@gmail.com>
2024-12-22 23:48:42 +0545

    added all the files

---
+++ image.js
@@ -0,0 +1,5 @@
+const searchForm = document.getElementById("search");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show_more-btn");
+
